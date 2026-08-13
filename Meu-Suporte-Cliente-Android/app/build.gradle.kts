8974d571d29dcb3eed301697e3931ec1362b481d
José Paz Junior <145562171+josepazjunior-dev@users.noreply.github.com>
2026-08-13 02:18:27 -0300

    Prepare Meu Suporte Onlline v2.0.1 for Play

--- Meu-Suporte-Cliente-Android/app/build.gradle.kts
+++ Meu-Suporte-Cliente-Android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "online.meusuporte.junioriptv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.0.1"
     }
     buildTypes {
         release {
