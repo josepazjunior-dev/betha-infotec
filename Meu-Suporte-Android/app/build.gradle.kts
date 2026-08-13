01af1febe3a6b980a2ce78da103ecc38cdbe7105
José Paz Junior <145562171+josepazjunior-dev@users.noreply.github.com>
2026-08-13 00:43:15 -0300

    Bump Meu Suporte Android to 1.0.1

--- Meu-Suporte-Android/app/build.gradle.kts
+++ Meu-Suporte-Android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "online.meusuporte.painel"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
