7e7f43bd6742a9b114744f5242db735712d40ab8
José Paz Junior <145562171+josepazjunior-dev@users.noreply.github.com>
2026-08-15 15:04:26 -0300

    Bump panel Android app to v1.0.4

--- Meu-Suporte-Android/app/build.gradle.kts
+++ Meu-Suporte-Android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "online.meusuporte.painel"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
 
     buildTypes {
